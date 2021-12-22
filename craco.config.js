14a768b3c2fc537e6d1ac40d9d7a00460b4e34ab
Aidan <aidan@fyelabs.com>
2021-12-22 12:13:51 -0500

    setup

---
+++ craco.config.js
@@ -0,0 +1,26 @@
+const CracoLessPlugin = require("craco-less");
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              "@primary-color": "#f2f2f2",
+              "@text-color": "#413f40",
+              "@text-color-secondary": "#ffffff",
+              '@box-shadow-base': '0 2px 8px 0 rgba(99, 99, 99, 0.2)',
+              'border-color-base': '#E8E8E8',
+              '@border-radius-base': '5px',
+              '@heading-color': "#4A5568",
+              '@success-color': '#97D710',
+            },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
